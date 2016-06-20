8ffe4e1825485a2ed8e156de466a7fff99f8291c
Kevin Old <kevin@kevinold.com>
2016-06-20 09:35:17 -0500

    fix remaining linting js and css issues

--- src/client/js/app/components/Greeting.js
+++ src/client/js/app/components/Greeting.js
@@ -1,17 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import HomeIcon from '../../../img/sprite/home.svg';
 
-class Greeting extends Component {
-  render() {
-    return (
-      <div>
-        <svg>
-          <use xlinkHref={HomeIcon} />
-        </svg>
-        <span>Hello world</span>
-      </div>
-    );
-  }
+function Greeting() {
+  return (
+    <div>
+      <svg>
+        <use xlinkHref={HomeIcon} />
+      </svg>
+      <span>Hello world</span>
+    </div>
+  );
 }
 
 export default Greeting;
