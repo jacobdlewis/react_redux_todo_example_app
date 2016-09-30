5ff9796cefcb42dcd8433f1cdb80d8c4942509bf
Kevin Old <kevin@kevinold.com>
2016-09-30 10:02:48 -0500

    update example components with tests for boilerplate

---
+++ src/client/js/app/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import acss from '../utils/acss';
+
+export default class Home extends React.Component {
+  render() {
+    const attrs = {
+      div: {
+        className: acss('Bgc(white)')
+      },
+      h1: {
+        className: acss('Fw(lr)', 'Fz(1.5em)', 'Fz(1.75em)--sm', 'Mb(gutter)', 'Mt(0)', 'Mx(a)')
+      },
+    };
+
+    return (
+      <div {...attrs.div}>
+        <h1 {...attrs.h1}>Home</h1>
+      </div>
+    );
+  }
+}
