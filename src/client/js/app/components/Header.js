f58db0bcddfd1fb2e224405f8662b5367287d178
Jacob Lewis <jacob@reax.io>
2016-12-31 15:59:59 -0600

    Update header styling.

--- src/client/js/app/components/Header.js
+++ src/client/js/app/components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import acss from '../utils/acss';
+import HomeIcon from '../../../img/sprite/home.svg';
 
 export default class Header extends React.Component {
   render() {
@@ -8,11 +9,21 @@ export default class Header extends React.Component {
       header: {
         className: acss('Bxsh(headerBoxShadow)', 'D(f)', 'Flxs(0)', 'H(headerHeight)', 'Pos(r)', 'Z(zHead)'),
       },
+      h1: {
+        className: acss('My(0)', 'P(10)')
+      },
+      svg: {
+        className: acss('M(10px)', 'Mt(15px)')
+      }
     };
 
     return (
       <header {...attrs.header}>
-        Header {isNotMobile}
+        <svg {...attrs.svg}>
+          <use xlinkHref={HomeIcon} />
+        </svg>
+    <h1 {...attrs.h1}>Odot</h1>
+        {isNotMobile}
       </header>
     );
   }
